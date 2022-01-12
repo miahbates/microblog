392eacc9b2acecd9452194e08e8c78e83f90c891
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2022-01-12 15:07:15 +0000

    server.js to serve homepage
    + Pass find home url
    + Pass find h1
    + Pass Submit form
    + Pass redirect

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -4,28 +4,29 @@ it("can find homepage", () => {
   cy.visit("/");
 });
 
-// check title of landing page 
+// check title of landing page
 it("can find title on home page", () => {
   cy.visit("/");
-  cy.get("h1").contains("momsnet");
+  cy.get("h1").contains("Moo-net");
 });
 
 // check form fields are filled out correctly 'can submit form'
 it("can submit form", () => {
   cy.visit("/");
-  cy.get("form").find("input[name='user-name']").type("moms74");
-  cy.get("form").find("input[name='message']").type("moms74");
+  cy.get("form").find("input[name='user-name']").type("mo074");
+  cy.get("form").find("input[name='message']").type("moo74");
   cy.get("form").find("input[type='submit']").click();
 });
 
-// form validation 
-// check 
+// form validation
+// check
 
-// redirects to home page 
-it("can submit form", () => {
+// redirects to home page
+
+it("can redirect", () => {
   cy.visit("/");
-  cy.get("form").find("input[name='user-name']").type("moms74");
-  cy.get("form").find("input[name='message']").type("moms74");
+  cy.get("form").find("input[name='user-name']").type("moos74");
+  cy.get("form").find("input[name='message']").type("moos74");
   cy.get("form").find("input[type='submit']").click();
   cy.url().should("include", "/");
 });
